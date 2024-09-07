b083c520f4d6a655e8b9a6054d8813f414478a94
rodi38 <rodrigorochaed1@gmail.com>
2024-09-07 01:12:29 -0300

    feat: initial customer views impl

---
+++ resources/views/customers/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="d-flex justify-content-between">
+    <h1>Listagem de Clientes</h1>
+
+    <div>
+        <a href="/customers/create" class="btn btn-success  btn-sm mx-3">Novo Cliente</a>
+
+    </div>
+
+</div>
+
+<table class="table table-striped table-bordered table-hover">
+    <thead class="thead-dark">
+        <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Criado em</th>
+            <th>Atualizado em</th>
+            <th>Açoes</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach($customers as $customer)
+            <tr>
+                <td>{{ $customer->id }}</td>
+                <td>{{ $customer->name }}</td>
+                <td>{{ $customer->email }}</td>
+                <td>{{ $customer->created_at }}</td>
+                <td>{{ $customer->updated_at }}</td>
+                <td>
+                    <a href="{{ route('customers.edit', $customer->id) }}" class="btn btn-warning  btn-sm">Editar</a>
+                    <form action="{{ route('customers.destroy', $customer->id) }}" method="POST" style="display:inline;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm">Excluir</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
+@endsection
